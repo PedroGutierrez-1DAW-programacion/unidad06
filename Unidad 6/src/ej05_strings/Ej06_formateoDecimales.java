43bd64e5867afd2372b52f316a4647064146c8ac
Pedro-vk <pedrogp9_6_93@hotmail.com>
2014-04-25 18:41:13 +0200

    Añadido ej06 - formateo de decimales

---
+++ Unidad 6/src/ej05_strings/Ej06_formateoDecimales.java
@@ -0,0 +1,13 @@
+package ej05_strings;
+
+public class Ej06_formateoDecimales {
+
+	public static void main(String[] args) {
+
+		float numero = 120092.20103f;
+		
+		System.out.printf("%,.4f", numero);
+
+	}
+
+}
